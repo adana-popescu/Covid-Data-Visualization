976cd1130b8494cd70dd702ecc14ca9e20205ff7
Adana Popescu <adana.popescu22@gmail.com>
2022-05-14 02:40:52 +0300

    finished crawler and csv writer

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import requests
+import csv
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+URL = "https://www.worldometers.info/coronavirus/"
+response = requests.get(URL).text
+soup = BeautifulSoup(response, 'html.parser')
 
+table = soup.find('table', id="main_table_countries_today")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+rows = table.find_all('tr')
 
+with open('covid-info.csv', 'w', encoding="UTF-8", newline="") as file:
+    writer = csv.writer(file)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    for row in rows:
+        count = 0
+        cells = row.find_all(['th', 'td'])
+        country = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        for cell in cells:
+            cell_text = cell.text.replace("\n", '')
+            country.append(cell_text)
+
+            count += 1
+            if count == 14:
+                break
+        if country[0] != "":
+            writer.writerow(country)
