003caaf00601c6e94f17da2fef1a7f9a07e20ea6
Adana Popescu <adana.popescu22@gmail.com>
2022-05-14 13:05:51 +0300

    added selection options

--- app.py
+++ app.py
@@ -5,14 +5,23 @@ import plotly.express as px
 pie_chart_cols = ['Country,Other', 'TotalDeaths', 'TotalRecovered']
 df = pd.read_csv('covid-info.csv', usecols=pie_chart_cols)
 
-options = df['Country,Other'].tolist()
+options = ["All"] + df['Country,Other'].tolist()
 selection = st.selectbox('Country: ', options)
 
-df.set_index("Country,Other", inplace=True)
-data = df.loc[selection].tolist()
+if selection == "All":
+    st.write(df)
+else:
+    chart_options = ["Pie", "Bar", "Plot", "Map"]
+    chart_selection = st.selectbox('Chart type:', chart_options)
 
-labels = ['value', 'type']
+    if chart_selection == "Pie":
+        df.set_index("Country,Other", inplace=True)
+        data = df.loc[selection].tolist()
 
-pie_df = pd.DataFrame([(int(data[0].replace(',', '')), "Total Recovered"), (int(data[1].replace(',', '')), "Total Deaths")], columns=labels)
-fig = px.pie(pie_df, values='value', names='type', title='ad')
-st.plotly_chart(fig)
+        labels = ['value', 'type']
+
+        pie_df = pd.DataFrame(
+            [(int(data[0].replace(',', '')), "Total Recovered"), (int(data[1].replace(',', '')), "Total Deaths")],
+            columns=labels)
+        fig = px.pie(pie_df, values='value', names='type', title='ad')
+        st.plotly_chart(fig)
