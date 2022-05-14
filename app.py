b818b3c48d80350a22a059de3ad5bd90874d70bb
Craciun Razvan-Marian <razvanmariancr@gmail.com>
2022-05-14 13:11:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import plotly.express as px
 
 pie_chart_cols = ['Country,Other', 'TotalDeaths', 'TotalRecovered']
 df = pd.read_csv('covid-info.csv', usecols=pie_chart_cols)
+df.fillna('0', inplace=True)
 
 options = ["All"] + df['Country,Other'].tolist()
 selection = st.selectbox('Country: ', options)
@@ -23,5 +24,5 @@ else:
         pie_df = pd.DataFrame(
             [(int(data[0].replace(',', '')), "Total Recovered"), (int(data[1].replace(',', '')), "Total Deaths")],
             columns=labels)
-        fig = px.pie(pie_df, values='value', names='type', title='ad')
+        fig = px.pie(pie_df, values='value', names='type')
         st.plotly_chart(fig)
